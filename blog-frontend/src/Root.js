ea11a228ba9ed903322bd6f004417553bdb5f69f
LeeSeul <mary567@hanmail.net>
2019-10-21 16:34:22 +0900

    1

--- blog-frontend/src/Root.js
+++ blog-frontend/src/Root.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
+import { provider } from 'react-redux';
+import configure from 'store/configure';
+
+const store = configure();
 
 const Root = () => {
     return (
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </provider>
     );
 }
 
